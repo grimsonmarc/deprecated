1239e1912407b26dbe35ef2e91058a3a48044c70
grimsonmarc <grimson.marc@gmail.com>
2014-12-19 10:54:08 -0800

    Remove deprecated pass by references

--- Crypt/RSA/Crypt_RSA_MathLoader.php
+++ Crypt/RSA/Crypt_RSA_MathLoader.php
@@ -71,7 +71,7 @@ class Crypt_RSA_MathLoader
      *
      * @access public
      */
-    function &loadWrapper($wrapper_name = 'default')
+    function loadWrapper($wrapper_name = 'default')
     {
         static $math_objects = array();
         // ordered by performance. GMP is the fastest math library, BCMath - the slowest.
@@ -85,17 +85,17 @@ class Crypt_RSA_MathLoader
             return $math_objects[$wrapper_name];
         }
 
-        $err_handler = &new Crypt_RSA_ErrorHandler;
+        $err_handler = new Crypt_RSA_ErrorHandler;
 
         if ($wrapper_name === 'default') {
             // try to load the most suitable wrapper
             $n = sizeof($math_wrappers);
             for ($i = 0; $i < $n; $i++) {
-                $obj = &Crypt_RSA_MathLoader::loadWrapper($math_wrappers[$i]);
+                $obj = Crypt_RSA_MathLoader::loadWrapper($math_wrappers[$i]);
                 if (!$err_handler->isError($obj)) {
                     // wrapper for $math_wrappers[$i] successfully loaded
                     // register it as default wrapper and return reference to it
-                    return $math_objects['default'] = &$obj;
+                    return $math_objects['default'] = $obj;
                 }
             }
             // can't load any wrapper
@@ -104,24 +104,23 @@ class Crypt_RSA_MathLoader
         }
 
         $class_name = 'Crypt_RSA_Math_' . $wrapper_name;
-        $class_filename = dirname(__FILE__) . '/Math/' . $wrapper_name . '.php';
 		
 		$class_name = "\\Deprecated\\Crypt\\RSA\\" . $class_name;
 		
         if (!class_exists($class_name)) {
-            $err_handler->pushError("can't find class [{$class_name}] in file [{$class_filename}]", CRYPT_RSA_ERROR_NO_CLASS);
+            $err_handler->pushError("can't find class [{$class_name}]", CRYPT_RSA_ERROR_NO_CLASS);
             return $err_handler->getLastError();
         }
 		
         // create and return wrapper object on success or PEAR_Error object on error
-        $obj = &new $class_name;
+        $obj = new $class_name;
         if ($obj->errstr) {
             // cannot load required extension for math wrapper
             $err_handler->pushError($obj->errstr, CRYPT_RSA_ERROR_NO_EXT);
             return $err_handler->getLastError();
         }
 		
-        return $math_objects[$wrapper_name] = &$obj;
+        return $math_objects[$wrapper_name] = $obj;
     }
 }
 
